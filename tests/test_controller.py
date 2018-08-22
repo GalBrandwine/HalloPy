e3d15f90f5e650a5229f692006106a8528bc66cb
GalBrandwine <gal080592@gmail.com>
2018-08-23 00:27:49 +0300

    updating license

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,7 +1,24 @@
 from unittest import TestCase
 from hallopy.controller import Controller
 
+"""Mockito usage.
 
+from mockito import when, mock, unstub
+
+when(os.path).exists('/foo').thenReturn(True)
+
+# or:
+import requests  # the famous library
+# you actually want to return a Response-like obj, we'll fake it
+response = mock({'status_code': 200, 'text': 'Ok'})
+when(requests).get(...).thenReturn(response)
+
+# use it
+requests.get('http://google.com/')
+
+# clean up
+unstub()
+"""
 class TestController(TestCase):
     def test_get_up_param(self):
         """Test if up param above 0. """
