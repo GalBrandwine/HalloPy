d4b67b1e8eb018fec86d78c1a2e201151a53c7f0
GalBrandwine <gal080592@gmail.com>
2018-09-10 12:18:29 +0300

    added Tracker class, for tracking points of interest after flags_handler.calibrated is True.

--- util/image_comp_tool.py
+++ util/image_comp_tool.py
@@ -118,3 +118,29 @@ class ImageTestTool:
                 temp_y = point[0][1]
 
         return point[0][0], point[0][1]
+
+    @staticmethod
+    def get_contour_extreme_points(contour, image=None, draw=False):
+        c = contour
+        # determine the most extreme points along the contour
+        extLeft = tuple(c[c[:, :, 0].argmin()][0])
+        extRight = tuple(c[c[:, :, 0].argmax()][0])
+        extTop = tuple(c[c[:, :, 1].argmin()][0])
+        extBot = tuple(c[c[:, :, 1].argmax()][0])
+
+        if draw is True:
+            try:
+                # draw the outline of the object, then draw each of the
+                # extreme points, where the left-most is red, right-most
+                # is green, top-most is blue, and bottom-most is teal
+                cv2.drawContours(image, [c], -1, (0, 255, 255), 2)
+                cv2.circle(image, extLeft, 8, (0, 0, 255), -1)
+                cv2.circle(image, extRight, 8, (0, 255, 0), -1)
+                cv2.circle(image, extTop, 8, (255, 0, 0), -1)
+                cv2.circle(image, extBot, 8, (255, 255, 0), -1)
+                cv2.imshow('contour_and_extreme_points', image)
+                cv2.waitKey()
+            except cv2.error as error:
+                print(error)
+
+        return extLeft, extRight, extTop, extBot
